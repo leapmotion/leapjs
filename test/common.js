4ab6e1a54dcedf567b53a05ca3a31c7000599e50
Jared Deckard <jared.deckard@gmail.com>
2013-04-25 16:17:58 -0500

    Updating Travis tests

--- test/common.js
+++ test/common.js
@@ -50,8 +50,8 @@ var fakeFrame = exports.fakeFrame = function(opts) {
     timestamp: frameId,
     pointables: _(opts.fingers || 0).times(function() { return fakeFinger() }),
     hands: opts.handData || _(opts.hands || 0).times(function() { return fakeHand() }),
-    r: opts.rotation || [[0,1,2], [2,3,4], [2,3,4]],
-    t: opts.translation || [1, 2, 3],
+    r: opts.rotation || new Leap.Matrix([[0,1,2], [2,3,4], [2,3,4]]),
+    t: opts.translation || new Leap.Vector([1, 2, 3]),
   };
   if (opts.gestures) {
     frame.gestures = opts.gestures;
@@ -73,8 +73,8 @@ var fakeHand = exports.fakeHand = function(opts) {
     id: handId - 1,
     valid: true,
     palm: [],
-    r: (opts && opts.rotation) || [[0,1,2], [2,3,4], [2,3,4]],
-    t: (opts && opts.translation) || [1, 2, 3]
+    r: (opts && opts.rotation) || new Leap.Matrix([[0,1,2], [2,3,4], [2,3,4]]),
+    t: (opts && opts.translation) || new Leap.Vector([1, 2, 3])
   }
 }
 
@@ -87,9 +87,9 @@ var fakeFinger = exports.fakeFinger = function() {
     width: 5,
 
     tip: {
-      direction: [10, 10, 10],
-      position: [10, 10, 10],
-      direction: [10, 10, 10]
+      direction: new Leap.Vector([10, 10, 10]),
+      position: new Leap.Vector([10, 10, 10]),
+      direction: new Leap.Vector([10, 10, 10])
     }
   }
 }
--- test/motion.js
+++ test/motion.js
@@ -1,28 +1,21 @@
 describe('Motion', function(){
   describe('Frame', function() {
-    describe('#matrix()', function(){
-      it('should return a rotation matrix', function(){
-        var data1 = fakeFrame({rotation: [[0,1,2], [2,3,4], [2,3,4]]})
-        var frame1 = new Leap.Frame(data1);
-        assert.deepEqual([[0,1,2],[2,3,4],[2,3,4]], frame1.matrix())
-      })
-    })
 
     describe('#translation()', function(){
       it('should return the translation', function(){
-        var data1 = fakeFrame({translation: [1, 2, 3]})
+        var data1 = fakeFrame({translation: new Leap.Vector([1, 2, 3])})
         var frame1 = new Leap.Frame(data1);
-        var data2 = fakeFrame({translation: [3, 1, 5]})
+        var data2 = fakeFrame({translation: new Leap.Vector([3, 1, 5])})
         var frame2 = new Leap.Frame(data2);
-        assert.deepEqual([-2, 1, -2], frame1.translation(frame2))
+        assert.deepEqual(new Leap.Vector([-2, 1, -2]), frame1.translation(frame2))
       })
     })
 
     describe('#rotationAxis()', function(){
       it('should return the rotationAxis', function(){
-        var data1 = fakeFrame({rotation: [[0,1,2], [2,3,4], [2,3,4]]})
+        var data1 = fakeFrame({rotation: new Leap.Matrix([[0,1,2], [2,3,4], [2,3,4]])})
         var frame1 = new Leap.Frame(data1);
-        var data2 = fakeFrame({rotation: [[0,4,5], [1,3,7], [5,4,2]]})
+        var data2 = fakeFrame({rotation: new Leap.Matrix([[0,4,5], [1,3,7], [5,4,2]])})
         var frame2 = new Leap.Frame(data2);
         var result = frame1.rotationAxis(frame2);
         assert.closeTo(-0.7427813, result[0], 0.0001)
@@ -33,9 +26,9 @@ describe('Motion', function(){
 
     describe('#rotationAngle()', function(){
       it('should return the rotationAngle', function(){
-        var data1 = fakeFrame({rotation: [[1.0,0.0,0.0], [0.0,1.0,0.0], [0.0,0.0,1.0]]})
+        var data1 = fakeFrame({rotation: new Leap.Matrix([[1.0,0.0,0.0], [0.0,1.0,0.0], [0.0,0.0,1.0]])})
         var frame1 = new Leap.Frame(data1);
-        var data2 = fakeFrame({rotation: [[1.0,0.0,0.0], [0.0,1.0,0.0], [0.5,0.0,0.5]]})
+        var data2 = fakeFrame({rotation: new Leap.Matrix([[1.0,0.0,0.0], [0.0,1.0,0.0], [0.5,0.0,0.5]])})
         var frame2 = new Leap.Frame(data2);
         var result = frame1.rotationAngle(frame2);
         assert.closeTo(0.72273, result, 0.0001)
@@ -44,29 +37,22 @@ describe('Motion', function(){
   })
 
   describe('Hand', function() {
-    describe('#matrix()', function(){
-      it('should return a rotation matrix', function(){
-        var data1 = fakeFrame({handData: [fakeHand({rotation: [[0,1,2], [2,3,4], [2,3,4]]})]})
-        var frame1 = new Leap.Frame(data1);
-        assert.deepEqual([[0,1,2],[2,3,4],[2,3,4]], frame1.hand(0).matrix())
-      })
-    })
 
     describe('#translation()', function(){
       it('should return the translation', function(){
-        var data1 = fakeFrame({handData: [fakeHand({translation: [1, 2, 3]})]})
+        var data1 = fakeFrame({handData: [fakeHand({translation: new Leap.Vector([1, 2, 3])})]})
         var frame1 = new Leap.Frame(data1);
-        var data2 = fakeFrame({handData: [fakeHand({translation: [3, 1, 5]})]})
+        var data2 = fakeFrame({handData: [fakeHand({translation: new Leap.Vector([3, 1, 5])})]})
         var frame2 = new Leap.Frame(data2);
-        assert.deepEqual([-2, 1, -2], frame1.hand(0).translation(frame2.hand(0)))
+        assert.deepEqual(new Leap.Vector([-2, 1, -2]), frame1.hand(0).translation(frame2.hand(0)))
       })
     })
 
     describe('#rotationAxis()', function(){
       it('should return the rotationAxis', function(){
-        var data1 = fakeFrame({handData: [fakeHand({rotation: [[0,1,2], [2,3,4], [2,3,4]]})]})
+        var data1 = fakeFrame({handData: [fakeHand({rotation: new Leap.Matrix([[0,1,2], [2,3,4], [2,3,4]])})]})
         var frame1 = new Leap.Frame(data1);
-        var data2 = fakeFrame({handData: [fakeHand({rotation: [[0,4,5], [1,3,7], [5,4,2]]})]})
+        var data2 = fakeFrame({handData: [fakeHand({rotation: new Leap.Matrix([[0,4,5], [1,3,7], [5,4,2]])})]})
         var frame2 = new Leap.Frame(data2);
         var result = frame1.hand(0).rotationAxis(frame2.hand(0));
         assert.closeTo(-0.74278, result[0], 0.0001)
@@ -77,9 +63,9 @@ describe('Motion', function(){
 
     describe('#rotationAngle()', function(){
       it('should return the rotationAngle', function(){
-        var data1 = fakeFrame({handData: [fakeHand({rotation: [[1,0,0], [0,1,0], [0,0,1]]})]})
+        var data1 = fakeFrame({handData: [fakeHand({rotation: new Leap.Matrix([[1,0,0], [0,1,0], [0,0,1]])})]})
         var frame1 = new Leap.Frame(data1);
-        var data2 = fakeFrame({handData: [fakeHand({rotation: [[1,0,0], [0,1,0], [0.5,0,0.5]]})]})
+        var data2 = fakeFrame({handData: [fakeHand({rotation: new Leap.Matrix([[1,0,0], [0,1,0], [0.5,0,0.5]])})]})
         var frame2 = new Leap.Frame(data2);
         var result = frame1.hand(0).rotationAngle(frame2.hand(0));
         assert.closeTo(0.72273, result, 0.0001)
