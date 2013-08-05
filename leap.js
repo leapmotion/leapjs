4e970af61b8d5e9b47a218a48b9f148c86a36eab
Josh Hull <joshbuddy@gmail.com>
2013-08-05 02:20:51 -0700

    remove extra window.

--- leap.js
+++ leap.js
@@ -6791,7 +6791,7 @@ if (typeof(window.requestAnimationFrame) !== 'function') {
     window.mozRequestAnimationFrame      ||
     window.oRequestAnimationFrame        ||
     window.msRequestAnimationFrame       ||
-    function(callback) { window.setTimeout(callback, 1000 / 60); }
+    function(callback) { setTimeout(callback, 1000 / 60); }
   })();
 }
 
