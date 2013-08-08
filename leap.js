bf97f68831d56fc4bb150dd7b1d8facd597c8d4c
Alan Davis <adavis@leapmotion.com>
2013-08-07 19:48:20 -0700

    Remove unnecessary closure

--- leap.js
+++ leap.js
@@ -6790,14 +6790,13 @@ if(typeof(exports) !== 'undefined') {
 })()
 },{}],21:[function(require,module,exports){
 if (typeof(window.requestAnimationFrame) !== 'function') {
-  window.requestAnimationFrame = (function() {
-    return
+  window.requestAnimationFrame = (
     window.webkitRequestAnimationFrame   ||
     window.mozRequestAnimationFrame      ||
     window.oRequestAnimationFrame        ||
     window.msRequestAnimationFrame       ||
     function(callback) { setTimeout(callback, 1000 / 60); }
-  })();
+  );
 }
 
 Leap = require("../lib/index");
--- template/entry.js
+++ template/entry.js
@@ -1,12 +1,11 @@
 if (typeof(window.requestAnimationFrame) !== 'function') {
-  window.requestAnimationFrame = (function() {
-    return
+  window.requestAnimationFrame = (
     window.webkitRequestAnimationFrame   ||
     window.mozRequestAnimationFrame      ||
     window.oRequestAnimationFrame        ||
     window.msRequestAnimationFrame       ||
     function(callback) { setTimeout(callback, 1000 / 60); }
-  })();
+  );
 }
 
 Leap = require("../lib/index");
